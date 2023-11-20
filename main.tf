fb3be2d83839e32e7931e1cafd2710d62ba68034
SYSHIL <ilhan.syed@gmail.com>
2023-11-20 21:11:14 +0530

    initial

--- main.tf
+++ main.tf
@@ -7,43 +7,46 @@ terraform {
   }
 }
 
-provider "tfe" {
-  hostname = "app.terraform.io"
+provider "tfe" {}
+
+resource "tfe_organization" "revdev" {
+  name                     = "revdev"
+  email                    = "ilhan.syed@gmail.com"
+  collaborator_auth_policy = "password"
+  cost_estimation_enabled  = true
 }
 
-data "tfe_oauth_client" "client" {
-  organization = "revdev"
-  name         = "revdev"
-}
 
-data "terraform_remote_state" "bootstrap" {
-  backend = "remote"
 
-  config = {
-    organization = "your-organization-name"
-    workspaces = {
-      name = "bootstrap"
-    }
-  }
-}
+# data "tfe_oauth_client" "client" {
+#   organization = "revdev"
+#   name         = "revdev"
+# }
 
-output "oauth_token_id" {
-  value = data.tfe_oauth_client.client.oauth_token_id
-}
+# data "terraform_remote_state" "bootstrap" {
+#   backend = "remote"
 
-resource "aws_instance" "example" {
-  ami           = "ami-0c94855ba95c71c99"
-  instance_type = "t2.micro"
-}
+#   config = {
+#     organization = "your-organization-name"
+#     workspaces = {
+#       name = "bootstrap"
+#     }
+#   }
+# }
 
-resource "tfe_workspace" "example" {
-  for_each = toset(["workspace1", "workspace2"]) // Add more workspace names here
+# output "oauth_token_id" {
+#   value = data.tfe_oauth_client.client.oauth_token_id
+# }
 
-  name        = each.key
-  organization = "your-organization-name"
-  vcs_repo {
-    identifier = "your-github-username/tf-github"
-    display_identifier = "your-github-username/tf-github"
-    oauth_token_id = data.tfe_oauth_client.client.oauth_token_id
-  }
-}
+
+# resource "tfe_workspace" "example" {
+#   for_each = toset(["workspace1", "workspace2"]) // Add more workspace names here
+
+#   name        = each.key
+#   organization = "your-organization-name"
+#   vcs_repo {
+#     identifier = "your-github-username/tf-github"
+#     display_identifier = "your-github-username/tf-github"
+#     oauth_token_id = data.tfe_oauth_client.client.oauth_token_id
+#   }
+# }
